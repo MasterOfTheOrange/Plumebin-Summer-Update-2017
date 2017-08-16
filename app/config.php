35690543760e2d939aae20cff8d283b88955f24e
Emilio s. Uribe <emilio.s.uribe@gmail.com>
2017-08-15 19:30:05 -0700

    Update config.php

--- app/config.php
+++ app/config.php
@@ -2,9 +2,9 @@
 
 return $config = [
 
-  'account_sid' => 'AC2637b9b41b018971e735a9e99af4c7dd',
-  'auth_token'  => '6af02ed1f632077ad4da863983144d14',
-  'phone_number'=> '5104221435'
+  'account_sid' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  'auth_token'  => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  'phone_number'=> '5xxxxxxxxxx'
 
 ];
 
